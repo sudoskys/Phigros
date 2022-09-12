82fc48dd4859b0acce30d3545a07076814b7d2b7
Jasmine <coldlando@hotmail.com>
2022-09-12 09:17:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ for idx, item in enumerate(ul_data):
             at_combo = 0
             at_charter = 0
 
-
+        print(f"写出了{go(song)}的{go(composer)}条目")
         data_list[go(song) + "_" + go(composer)] = {
                                                        "song": go(song),
                                                        "illustration": go(illustration),
@@ -139,5 +139,6 @@ for idx, item in enumerate(ul_data):
                                                    },
 data = json.dumps(data_list, sort_keys=True, indent=4, separators=(',', ':'))
 
-with open("Phigros.json", 'w+') as f:  # 如果filename不存在会自动创建， 'w'表示写数据，写之前会清空文件中的原有数据！
+with open("Phigros.json", 'w+') as f:  
+    print("写出了数据")
     f.write(data)
